33caf8acd75ba9737c7b35180d82def94b39fed7
chris Farah <farahca@mail.uc.edu>
2024-11-05 15:13:42 -0500

    added text to projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import Header from "../components/Header";
-import { ProjectsPic } from "../assets";
 import Project from "../components/Project";
 import { projects } from "../constants";
 import Footer from "../components/Footer";
 
 const Projects = () => {
-  console.log(projects);
+
   return (
     <>
       <Header homepage={false} />
       {projects.map((item) => (
-        <Project
-          key={item.id}
-          title={item.title}
-          link={item.link}
-          text={item.text}
-          picture={item.picture}
+        <Project {...item}
+          key={item.index}
         />
       ))}
       <Footer />
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,19 +5,13 @@ const Project = ({ title, link, picture, text }) => {
   return (
     <div className="pt-2 w-full flex flex-col">
       <line className="border-t w-[90%] border-black h-0 mx-auto" />
-      <div className="mt-3 w-full text-center items-center text-3xl">
+      <div className="mt-3 px-2 w-full text-center items-center text-3xl">
         {title}
       </div>
       <div className="w-full h-full flex flex-col lg:flex-row">
         <div className="w-full flex p-5 items-center text-center justify-center">
           <div className="">
-            <p className="m-5">{text}</p>
-            <Link
-              className=" text-Orange xl:text-2xl threexl:text-4xl underline text-xl pt-8 mr-5 flex flex-row-reverse hover:cursor-pointer"
-              to={link}
-            >
-              Learn More-&gt;
-            </Link>
+            {text.map((item)=>(<p className="mx-5 mt-2" key={item.index}>{item.message}</p>))}
           </div>
         </div>
         <div className="w-full flex justify-center items-center">
